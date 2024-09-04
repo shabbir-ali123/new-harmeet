9298ac340a43af98ad3634f0c2e58c7ac9e3e32c
shabbir-ali123 <shabbiralykhan123@gmail.com>
2024-09-04 06:36:44 -1200

    changes

---
+++ script.js
@@ -0,0 +1,67 @@
+document.querySelector(".hamburger").addEventListener("click", function () {
+  document.querySelector("nav ul").classList.toggle("show");
+});
+
+const sections = document.querySelectorAll("section");
+const navLinks = document.querySelectorAll("nav ul li a");
+
+window.addEventListener("scroll", () => {
+  let current = "";
+
+  sections.forEach((section) => {
+    const sectionTop = section.offsetTop;
+    const sectionHeight = section.clientHeight;
+    if (window.pageYOffset >= sectionTop - sectionHeight / 3) {
+      current = section.getAttribute("id");
+    }
+  });
+
+  navLinks.forEach((link) => {
+    link.classList.remove("active");
+    if (link.getAttribute("href").includes(current)) {
+      link.classList.add("active");
+    }
+  });
+});
+
+navLinks.forEach((link) => {
+  link.addEventListener("click", () => {
+    navLinks.forEach((link) => link.classList.remove("active"));
+    link.classList.add("active");
+  });
+
+  // portfolio sec
+
+  sections.forEach((section) => {
+    const sectionTop = section.offsetTop;
+    const sectionHeight = section.clientHeight;
+    if (window.pageYOffset >= sectionTop - sectionHeight / 3) {
+      current = section.getAttribute("id");
+    }
+  });
+
+  sections.forEach((link) => {
+    link.classList.remove("active");
+    if (link.getAttribute("href").includes(current)) {
+      link.classList.add("active");
+    }
+  });
+});
+
+sections.forEach((link) => {
+  link.addEventListener("click", () => {
+    navLinks.forEach((link) => link.classList.remove("active"));
+    link.classList.add("active");
+  });
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  const card5 = document.getElementById("card-5");
+  const seeAllBtn = document.getElementById("see-all-btn");
+
+  seeAllBtn.addEventListener("click", function (event) {
+    event.preventDefault();
+    card5.style.display = "block";
+    seeAllBtn.style.display = "none";
+  });
+});
